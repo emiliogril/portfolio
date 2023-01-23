babd8c4ab0586e7864ae4b84bc522eafd89914b4
Emilio Gril <emiliogril@MacBook-Pro-de-Emilio.local>
2023-01-23 20:03:50 -0300

    Subiendo portfolio

---
+++ src/components/Banner.js
@@ -0,0 +1,58 @@
+import { useState, useEffect } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import { ArrowRightCircle } from "react-bootstrap-icons";
+import headerImg from "../assets/img/header-img.svg"
+
+export const Banner = () => {
+    const [loopNum, setLoopNum] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(false);
+    const toRotate = ["Full Stack Developer", "Diseñador Grafico"];
+    const [text, setText] = useState('');
+    const [delta , setDelta] = useState(300 - Math.random() * 100); 
+    const period = 2000;
+
+    useEffect(() => {
+        let ticket = setInterval(() => {
+            tick();
+        },delta)
+        return () => { clearInterval(ticket)};
+    }, [text])
+
+    const tick = () => {
+        let i = loopNum % toRotate.length;
+        let fullText = toRotate[i];
+        let updateText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+
+        setText(updateText);
+
+        if (isDeleting) {
+            setDelta(prevDelta => prevDelta /2)
+        }
+
+        if (!isDeleting &&  updateText === fullText) {
+            setIsDeleting(true);
+            setDelta(period);
+        } else if (isDeleting && updateText === '') {
+            setIsDeleting(false);
+            setLoopNum(loopNum + 1);
+            setDelta(500);
+        }
+    }
+    return (
+        <section className="banner" id="home">
+            <Container>
+                <Row className="align-items-center">
+                    <Col xs={12} md={6} xl={7}>
+                        <span className="tagline">Bienvenido a mi Portfolio</span>
+                        <h1>{`Hola soy `}<span className="wrap">{text}</span></h1>
+                        <p>Soy hipoacúsico y me dedico a diseñar páginas web y otros trabajos de diseño en forma independiente. Mi meta personal es crecer en el terreno laboral, incrementar mis conocimientos en el área del diseño y programación de páginas webs, y poder también disfrutar del placer que da trabajar en lo que me gusta.</p>
+                        <button onClick={() => console.log('connect')}>Conectémonos <ArrowRightCircle size={25} /></button>
+                        </ Col>
+                    <Col xs={12} md={6} xl={5}>
+                        <img src={headerImg} alt="Headder img" /> 
+                    </Col>
+                </ Row>
+            </ Container>
+        </section>
+    )
+}
---
+++ src/components/Projects.js
@@ -0,0 +1,192 @@
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import projImg1 from "../assets/img/project-img1.png";
+import projImg2 from "../assets/img/project-img2.png";
+import projImg3 from "../assets/img/project-img3.png";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+
+import TrackVisibility from 'react-on-screen';
+
+export const Projects = () => {
+
+  const projects = [
+    {
+      title: "hola web",
+      description: "Design & Development",
+      imgUrl: projImg1,
+      thumbnail: "http://www.infobae.com",
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg3,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg3,
+    },
+  ];
+
+  const projects1 = [
+    {
+      title: "hola web 2",
+      description: "Design & Development",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg3,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg3,
+    },
+  ];
+
+  const projects2 = [
+    {
+      title: "hola web 3",
+      description: "Design & Development",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg3,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg3,
+    },
+  ];
+
+  return (
+    <section className="project" id="projects">
+      <Container>
+        <Row>
+          <Col size={12}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
+                <h2>Portfolio</h2>
+                <p>Si bien este sitio web es un proyecto. En esta sección añadiré nuevos trabajos a medida que vaya aplicando lo aprendido de manera autodidacta y/o en la carrera que actualmente curso.</p>
+                <Tab.Container id="projects-tabs" defaultActiveKey="disenio">
+                <Tab.Container id="projects1-tabs" defaultActiveKey="web">
+                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                    <Nav.Item>
+                      <Nav.Link eventKey="disenio">Diseño</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="web">Web</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="java">JavaScript</Nav.Link>
+                    </Nav.Item>
+                  </Nav>
+                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+                    <Tab.Pane eventKey="disenio">
+                    <p>Los trabajos de diseño graficos.</p>
+                      <Row>
+                        {
+                          projects.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="web">
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                      <Row>
+                        {
+                          projects1.map((projects1, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...projects1}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="java">
+                      <p>HOLAAA Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                      <Row>
+                        {
+                          projects2.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+                    </Tab.Pane>
+                  </Tab.Content>
+                </Tab.Container>
+                </Tab.Container>
+              </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+      <img className="background-image-right" src={colorSharp2}></img>
+    </section>
+  )
+}
